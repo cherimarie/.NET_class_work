9be3a3d21c587083bd6802d9e2f20afdb316f523
cherimarie <cherimarie@gmail.com>
2013-01-17 11:20:42 -0800

    calculator project complete

---
+++ Calculator/Default.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calculator</title>
+    <link href="CalculatorStyle.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server"> <!--all the ASP controls need to go in the form-->
+        <table>
+            <tr>
+                <td colspan="4"><asp:TextBox ID="TxtDisplay" runat="server" CssClass="numDisplay"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td><asp:Button ID="ClearButton" runat="server" Text="Cl" OnClick="ClearButton_Click" /> </td>
+                <td><asp:Button ID="DivideButton" runat="server" Text="/" OnClick="DivideButton_Click" /> </td>
+                <td><asp:Button ID="MultiplicationButton" runat="server" Text="*" OnClick="MultiplicationButton_Click" /> </td>
+                <td><asp:Button ID="MinusButton" runat="server" Text="-" OnClick="MinusButton_Click" /> </td>
+            </tr>
+             <tr>
+                <td><asp:Button ID="SevenButton" runat="server" Text="7" OnClick="inputNumber_click" /></td>
+                <td><asp:Button ID="EightButton" runat="server" Text="8" OnClick="inputNumber_click" /></td>
+                <td><asp:Button ID="NineButton" runat="server" Text="9" OnClick="inputNumber_click" /></td>
+                <td rowspan="2"><asp:Button ID="PlusButton" runat="server" Text="+" Height="43px" OnClick="PlusButton_Click"  /></td>
+            </tr>
+             <tr>
+                <td><asp:Button ID="FourButton" runat="server" Text="4" OnClick="inputNumber_click" /></td>
+                <td><asp:Button ID="FiveButton" runat="server" Text="5" OnClick="inputNumber_click" /></td>
+                <td><asp:Button ID="SixButton" runat="server" Text="6" OnClick="inputNumber_click" /></td>
+             </tr>
+             <tr>
+                <td><asp:Button ID="OneButton" runat="server" Text="1" OnClick="inputNumber_click" /></td>
+                <td><asp:Button ID="TwoButton" runat="server" Text="2" OnClick="inputNumber_click" /></td>
+                <td><asp:Button ID="ThreeButton" runat="server" Text="3" OnClick="inputNumber_click" /></td>
+                <td rowspan="2"><asp:Button ID="EqualsButton" runat="server" Text="=" Height="47px" OnClick="EqualsButton_Click" /></td>
+            </tr>
+             <tr>
+                <td colspan="2"><asp:Button ID="ZeroButton" runat="server" Text="0" OnClick="inputNumber_click" Width="50px" /></td>
+                <td><asp:Button ID="DecimalButton" runat="server" Text="." OnClick="inputNumber_click" /></td>
+            </tr>
+        </table>
+     </form>
+</body>
+</html>
---
+++ Calculator/Default.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    /// <summary>
+    /// Descr: This class implements a calculator! 
+    /// Date: Jan 10 2013
+    /// Prog: Cheri Allen 
+    /// </summary>
+
+    double number1 = 0; //the first string entered to textbox
+    double number2 = 0; //the second string enetered to textbox
+    double answer; //the solution that shows up after = is pressed
+    int sessionCheck = 1; //a flag to see if session is set, boolean could work too
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void inputNumber_click(object sender, EventArgs e)
+    { //all number buttons have this event 
+        Button b = (Button)sender;
+        TxtDisplay.Text += b.Text;
+    }
+
+    protected void ClearButton_Click(object sender, EventArgs e)
+    {
+        Clear();
+    }
+
+    protected void PlusButton_Click(object sender, EventArgs e)
+    {
+        GetNumbers();
+        Session["Solution"] = number1 + number2;
+        Session["Operator"] = "+";
+    }
+
+    protected void MinusButton_Click(object sender, EventArgs e)
+    {
+        GetNumbers();
+        if (sessionCheck == 0)
+        {
+            Session["Solution"] = number2;
+        }
+        else
+        {
+            Session["Solution"] = number1 - number2; 
+        }
+        Session["Operator"] = "-";
+    }
+
+    protected void MultiplicationButton_Click(object sender, EventArgs e)
+    {
+        GetNumbers();
+        if (sessionCheck == 0)
+        {
+            Session["Solution"] = number2;
+        }
+        else
+        {
+            Session["Solution"] = number1 * number2;
+        }
+        Session["Operator"] = "*";
+    }
+
+    protected void DivideButton_Click(object sender, EventArgs e)
+    {
+        GetNumbers();
+        if (sessionCheck == 0)
+        {
+            Session["Solution"] = number2;
+        }
+        else
+        {
+            Session["Solution"] = number1 * number2;
+        }
+        Session["Operator"] = "/";
+    }
+
+    protected void Clear()
+    {
+        TxtDisplay.Text = "";
+    }
+
+    protected void EqualsButton_Click(object sender, EventArgs e)
+    {
+        number1 = (double)Session["Solution"];
+        number2 = double.Parse(TxtDisplay.Text);
+        string op = Session["Operator"].ToString();
+
+        switch (op)
+        {
+            case "+":
+                answer = number1 + number2;
+                break;
+            case "-":
+                answer = number1 - number2;
+                break;
+            case "*":
+                answer = number1 * number2;
+                break;
+            case "/":
+                answer = number1 / number2;
+                break;
+        }
+        TxtDisplay.Text = answer.ToString();
+        Session["Solution"] = null;
+    }
+
+    protected void GetNumbers()
+    {
+        if (Session["Solution"] == null)
+        {
+            sessionCheck = 0;
+        }
+        else
+        {
+            number1 = (double)Session["Solution"];
+        }
+        bool isNumber = double.TryParse(TxtDisplay.Text, out number2);
+        if (!isNumber)
+        {
+         Clear();
+        return;
+        }
+        Clear();
+    
+    }
+
+   
+}
